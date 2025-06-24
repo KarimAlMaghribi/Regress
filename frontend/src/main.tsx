361ad86a44b7f5051576be837eae9d89fd478df0
KarimAlMaghribi <38104839+KarimAlMaghribi@users.noreply.github.com>
2025-06-24 20:06:19 +0200

    fix: add date picker context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 const root = createRoot(document.getElementById('root')!);
-root.render(<App />);
+root.render(
+  <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <App />
+  </LocalizationProvider>
+);
