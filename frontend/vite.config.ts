469853923c829ea40076e9bf0a514fe7e9bdd22c
KarimAlMaghribi <38104839+KarimAlMaghribi@users.noreply.github.com>
2025-07-26 18:32:15 +0300

    fix build issue with react-flow

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'react-flow-renderer': 'react-flow-renderer/dist/esm',
+    },
+  },
   server: {
     port: 3000,
   },
